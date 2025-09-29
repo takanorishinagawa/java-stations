01f92cf4ff33464d145e804dd837a6124d5968b3
takanorishinagawa <shinagawa.takanori@gmail.com>
2025-09-29 23:09:38 +0900

    station2 clear

--- src/main/java/com/example/station2/Main.java
+++ src/main/java/com/example/station2/Main.java
@@ -2,7 +2,8 @@ package com.example.station2;
 
 public class Main {
     public static void main(String[] args) {
-        // ここから
-        // ここまで
+        System.out.println(false);
+        System.out.println(42);
+        System.out.println(3.141592);
     }
 }
