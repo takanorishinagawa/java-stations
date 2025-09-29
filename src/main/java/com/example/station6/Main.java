e26a1dd998bf7d6272aae643b1bf4587969bd465
takanorishinagawa <shinagawa.takanori@gmail.com>
2025-09-30 00:15:34 +0900

    station6 clear

--- src/main/java/com/example/station6/Main.java
+++ src/main/java/com/example/station6/Main.java
@@ -2,7 +2,8 @@ package com.example.station6;
 
 public class Main {
     public static void main(String[] args) {
-        // ここから
-        // ここまで
+        int[] users = new int[]{ 23, 36, 31, 48, 58 };
+        int all = users[0] + users[1] + users[2] + users[3] + users[4];
+        System.out.println(all / 5.0);
     }
 }
