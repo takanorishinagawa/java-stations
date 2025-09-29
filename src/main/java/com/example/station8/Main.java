d2d326723fdd0897a85fa960b560743011ff8fa4
takanorishinagawa <shinagawa.takanori@gmail.com>
2025-09-30 01:05:17 +0900

    station8 clear

--- src/main/java/com/example/station8/Main.java
+++ src/main/java/com/example/station8/Main.java
@@ -2,15 +2,32 @@ package com.example.station8;
 
 public class Main {
     public static void question1(int subtotal) {
-        // Station 8 問題(1)
-        // ここから
-        // ここまで
+        int total = subtotal;
+        if (subtotal < 10000) {
+            System.out.println("送料800円を追加します");
+            total += 800;
+        }
+
+        System.out.println(total);
     }
 
     public static void question2(int categoryId) {
-        // Station 8 問題(2)
-        // ここから
-        // ここまで
+        switch (categoryId) {
+            case 1:
+                System.out.println("食品");
+                break;
+            case 2:
+                System.out.println("家電");
+                break;
+            case 3:
+                System.out.println("家具");
+                break;
+            case 99:
+                System.out.println("その他");
+                break;
+            default:
+                break;
+        }
     }
 
     public static void main(String[] args) {
