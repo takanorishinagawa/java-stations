71e2acac5a977885dac3ae2f5d4bcc8f4bb12f73
takanorishinagawa <shinagawa.takanori@gmail.com>
2025-09-30 08:08:52 +0900

    station9 clear

--- src/main/java/com/example/station9/Main.java
+++ src/main/java/com/example/station9/Main.java
@@ -2,9 +2,9 @@ package com.example.station9;
 
 public class Main {
     public static void main(String[] args) {
-        int[] itemPrices1 = {1000, 1100, 1200, 1300};
-        int[] itemPrices2 = {1000, 1100, 1200, 1300, 1600, 1800};
-        int[] itemPrices3 = {1000, 2100, 3200, 2300};
+        int[] itemPrices1 = { 1000, 1100, 1200, 1300 };
+        int[] itemPrices2 = { 1000, 1100, 1200, 1300, 1600, 1800 };
+        int[] itemPrices3 = { 1000, 2100, 3200, 2300 };
         System.out.print("カート内: 1000円, 1100円, 1200円, 1300円。期待されるクーポン: A。結果: ");
         test(itemPrices1);
         System.out.print("カート内: 1000円, 1100円, 1200円, 1300円, 1600円, 1800円。期待されるクーポン: B。結果: ");
@@ -14,7 +14,37 @@ public class Main {
     }
 
     public static void test(int[] itemPrices) {
-        // ここから
-        // ここまで
+        int totalA = 0;
+        for (int i = 0; i < itemPrices.length; i++) {
+            totalA += itemPrices[i];
+        }
+
+        int totalB = 0;
+        for (int i = 0; i < itemPrices.length; i++) {
+            totalB += itemPrices[i] * 0.9;
+        }
+        if (totalB < 5000) {
+            totalB += 800;
+        }
+
+        int totalC = 0;
+        for (int i = 0; i < itemPrices.length; i++) {
+            if (itemPrices[i] >= 2000) {
+                totalC += itemPrices[i] * 0.8;
+            } else {
+                totalC += itemPrices[i];
+            }
+        }
+        if (totalC < 5000) {
+            totalC += 800;
+        }
+
+        if (totalA <= totalB && totalA <= totalC) {
+            System.out.println("A");
+        } else if (totalB <= totalC && totalB <= totalA) {
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
     }
 }
