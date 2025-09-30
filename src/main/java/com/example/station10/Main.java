ea6c37ce9d0cde4c5845f62418bd044f5f207159
takanorishinagawa <shinagawa.takanori@gmail.com>
2025-09-30 21:33:59 +0900

    station10 clear

--- src/main/java/com/example/station10/Main.java
+++ src/main/java/com/example/station10/Main.java
@@ -2,12 +2,20 @@ package com.example.station10;
 
 public class Main {
     public static void main(String[] args) {
-        // テストする場合、以下の「//」を消してください。
-        //System.out.println("商品合計: 1000, 支払額: " + new Main().applyTaxAndShippingFee(1000));
-        //System.out.println("商品合計: 10000, 支払額: " + new Main().applyTaxAndShippingFee(10000));
-        //System.out.println("商品合計: 9999, 支払額: " + new Main().applyTaxAndShippingFee(9999));
+        System.out.println("商品合計: 1000, 支払額: " + new Main().applyTaxAndShippingFee(1000));
+        System.out.println("商品合計: 10000, 支払額: " + new Main().applyTaxAndShippingFee(10000));
+        System.out.println("商品合計: 9999, 支払額: " + new Main().applyTaxAndShippingFee(9999));
     }
 
-    // ここから
-    // ここまで
+    float applyTaxAndShippingFee(int noTaxSubtotal) {
+        float total = noTaxSubtotal;
+
+        if (noTaxSubtotal < 10000) {
+            total += 800;
+        }
+
+        total += total * 0.1;
+
+        return total;
+    }
 }
